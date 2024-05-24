dab41ce8891c781ed5a67dc887ee904c532a9dfc
hamoodi <alabbahs@gmail.com>
2024-05-24 15:20:31 +0100

    task 2 started

---
+++ Assessment-Fundamentals-Week-2-main/Level-2/task_two.py
@@ -0,0 +1,98 @@
+from datetime import date
+
+#####
+#
+# COPY YOUR CODE FROM LEVEL 1 BELOW
+#
+#####
+
+class Assessment:
+    def __init__(self, name: str, type: str, score: float):
+        self.name = name
+        self.type = type
+        self.score = score
+
+        if type not in ["multiple-choice", "technical", "presentation"]:
+            raise ValueError("Error! Invalid assessment type")
+
+        if not (0 <= score <= 100):
+            raise ValueError("Error! Assessment score is out of range 0-100")
+
+
+class Trainee:
+    def __init__(self, name: str, email: str, date_of_birth: date) -> None:
+        self.name = name
+        self.email = email
+        self.date_of_birth = date_of_birth
+        self.assessments: list[Assessment] = []
+
+    def get_age(self) -> int:
+        delta_time = date.today() - self.date_of_birth
+        days = delta_time.days
+        age = int(days//365.25)
+        return age
+
+    def add_assessment(self, assessment: Assessment) -> None:
+        if not isinstance(assessment, Assessment):
+            raise TypeError("Error! Tried to add non-assessment object to list")
+        self.assessments.append(assessment)
+
+    def get_assessment(self, name: str) -> Assessment | None:
+        for assessment in self.assessments:
+            if name == assessment.name:
+                return assessment
+        return None
+    
+    def get_assessment_of_type(self, type: str) -> list[Assessment]:
+        list_of_homogeneous_assessments = []
+        for assessment in self.assessments:
+            if assessment.type == type:
+                list_of_homogeneous_assessments.append()
+        return list_of_homogeneous_assessments
+
+
+
+#####
+#
+# COPY YOUR CODE FROM LEVEL 1 ABOVE
+#
+#####
+
+
+class MultipleChoiceAssessment(Assessment):
+    def __init__(self, name, score) -> None:
+        super().__init__(name, "multiple-choice", score)
+
+    def calculate_score(self):
+        score = self.score*0.7
+        return score
+
+
+class TechnicalAssessment(Assessment):
+    def __init__(self, name, type, score) -> None:
+        super().__init__(self, name, type, score)
+
+    def calculate_score(self):
+        return self.score
+
+class PresentationAssessment(Assessment):
+    def __init__(self, name, type, score) -> None:
+        super().__init__(self, name, type, score)
+
+    def calculate_score(self):
+        score = int(self.score*0.6)
+        return score
+
+
+if __name__ == "__main__":
+    trainee = Trainee("Sigma", "trainee@sigmalabs.co.uk", date(1990, 1, 1))
+    print(trainee)
+    print(trainee.get_age())
+    trainee.add_assessment(MultipleChoiceAssessment(
+        "Python Basics", 90.1))
+    trainee.add_assessment(TechnicalAssessment(
+        "Python Data Structures", 67.4))
+    trainee.add_assessment(MultipleChoiceAssessment("Python OOP", 34.3))
+    print(trainee.get_assessment("Python Basics"))
+    print(trainee.get_assessment("Python Data Structures"))
+    print(trainee.get_assessment("Python OOP"))
---
+++ Assessment-Fundamentals-Week-2-main/README.md
@@ -0,0 +1,30 @@
+# Tau Labs
+
+Welcome to Tau Labs! We are a high performance team of data scientists and engineers who are passionate about solving the world's most challenging problems with data. We are a team of 10,000+ members and we are growing every day.
+
+In this challenge you'll be building out our assessment framework. We have a number of different assessments that we use to assess candidates and we need to be able to run these assessments and store the results.
+
+## Setup
+
+Please ensure you do every step below carefully. Not doing so will mean we can't assess your work and **will result in a score of zero**.
+
+1. Create a repo named exactly `Assessment-Fundamentals-Week-2`
+2. Invite your coaches to it (they'll let you know they Github usernames)
+3. Push all the code in this folder to your created repository
+4. On your created repo, click on `Actions` in the top menu bar
+   - If it's there, click on `I understand my workflows, go ahead and enable them`
+5. Complete the assessment
+6. Commit & push your code regularly
+
+Before running your code, run this command
+
+> pip3 install -r requirements.txt
+
+## Files Explained
+
+In each of the `Level` folders you will find the code and instructions for that level. **Each level will build on the one before it**; be sure to read the instructions carefully.
+
+- `task_[level].py` - All of your code should be written in this file.
+- `test_[level]_task.py` - This file contains some basic tasks for this level. You should also add your own tests here
+- `README.md` - This file contains the instructions for the level.
+- `test_[level]_assessment.py` - **This file is encrypted.** It contains additional tests for this level that are used as part of the assessment. **You should not modify this file.**
